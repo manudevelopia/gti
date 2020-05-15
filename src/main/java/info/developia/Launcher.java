b856f404d8a289a2349453303878650e3ed8a1e0
manudevelopia <manugarciarodriguez@gmail.com>
2020-05-15 11:28:26 +0200

    Clean unused method

--- src/main/java/info/developia/Launcher.java
+++ src/main/java/info/developia/Launcher.java
@@ -4,10 +4,6 @@ import info.developia.gti.Injector;
 
 public class Launcher {
 
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
     public static void main(String[] args) {
         Injector injector = new Injector(Library.class);
         Library library = (Library) injector.getInstanceFor(Library.class);
