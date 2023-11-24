45c8de7ad208c8bfc53b68cd99cd1897ed2113b0
manudevelopia <manugarciarodriguez@gmail.com>
2023-11-24 17:07:54 +0100

    simplify code initialization

--- src/test/java/fixture/Launcher.java
+++ src/test/java/fixture/Launcher.java
@@ -1,19 +1,11 @@
 package fixture;
 
 import info.developia.gti.Gti;
-import info.developia.gti.Injector;
 
 public class Launcher {
 
-    @Injector
-    private final Library library;
-
-    public Launcher(Library library) {
-        this.library = library;
-    }
-
     public static void main(String[] args) {
-        var launcher = Gti.startOn(Launcher.class);
-        launcher.library.process();
+        var library = Gti.startOn(Library.class);
+        library.process();
     }
 }
